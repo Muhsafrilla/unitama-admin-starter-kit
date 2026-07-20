cbc9c1427518e00b6c99563b07562d42a2d05b4e
Muhsafrilla <locciidisini27@gmail.com>
2026-07-20 12:32:06 +0800

    Pertemuan 13 : Update, Delete & Show User

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,36 @@
+<div class="row g-3">
+    <div class="col-md-3">
+        <img src="{{ $user->avatar ? asset('storage/' . $user->avatar) : asset('niceadmin/img/noprofilpicture.png') }}"
+            alt="Avatar" class="w-100 rounded">
+    </div>
+
+    <div class="col-md-9">
+        <table class="table">
+            <tr>
+                <td width="10">Email</td>
+                <td width="3">:</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td>:</td>
+                <td>{{ $user->name }}</td>
+            </tr>
+            <tr>
+                <td>Role</td>
+                <td>:</td>
+                <td>{{ $user->role }}</td>
+            </tr>
+            <tr>
+                <td>Dibuat</td>
+                <td>:</td>
+                <td>{{ $user->created_at->diffForHumans() }}</td>
+            </tr>
+            <tr>
+                <td>Diubah</td>
+                <td>:</td>
+                <td>{{ $user->updated_at->diffForHumans() }}</td>
+            </tr>
+        </table>
+    </div>
+</div>
